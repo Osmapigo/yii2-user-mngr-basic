57772ac17abd49641ba3585393465353f66f4295
Oscar M. Piragauta <oscarl1718@gmail.com>
2016-03-05 22:19:44 -0500

    Added dabase table models for User and Person

---
+++ frontend/models/Person.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "person".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $phone_number
+ * @property string $fk_user
+ *
+ * @property User $fkUser
+ */
+class Person extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'person';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'fk_user'], 'required'],
+            [['name', 'fk_user'], 'string', 'max' => 128],
+            [['phone_number'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'phone_number' => 'Phone Number',
+            'fk_user' => 'Fk User',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFkUser()
+    {
+        return $this->hasOne(User::className(), ['email' => 'fk_user']);
+    }
+}
